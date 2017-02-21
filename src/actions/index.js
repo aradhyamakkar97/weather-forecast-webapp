f2ae47b283b25b30942ee0989a810ab9c2f24158
Aradhya Makkar <aradhyamakkar97@gmail.com>
2017-02-22 04:17:29 +0530

    using axios to make api request and create action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,20 @@
+import axios from 'axios';
 const API_KEY = '296d125687571b91664d5484442780be';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`
+
+export const FETCH_WEATHER='FETCH_WEATHER'
+
+export function fetchWeather(city){
+
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+
+
+  }
+}
